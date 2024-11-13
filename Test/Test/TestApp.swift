79278b715ec06f6e07e3658394a7c056b4e794be
Giusy Di Paola <giusydipaola@icloud.com>
2024-11-13 12:54:37 +0100

    Submite testing files

---
+++ Test/Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Giusy Di Paola on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
